e94c171ef158466b69612714dabecfd1929b7507
Harrison Beckerich <hbeckeri@purdue.edu>
2018-09-16 12:29:09 -0400

    develop updated proxy

--- example.js
+++ example.js
@@ -1,13 +1,11 @@
 const net = require('net');
 const StratumProxy = require('./index');
 
-const app = net.createServer(socket => {
-	const connection = new StratumProxy(socket);
-
-	connection.on('connected', () => console.log('connected'));
-	connection.on('data', data => console.log('data', data));
-	connection.on('error', error => console.log('error', error));
-	connection.on('disconnected', () => console.log('disconnected'));
+const app = new StratumProxy({
+	poolAddress: 'cryptonightheavy.usa.nicehash.com',
+	poolPort: 3364,
+	minerName: 'poolminer',
+	address: '3Js4sXXk9Ca8Y52CezUxuZcDugVx8NtrDT',
 });
 
 app.listen(3333, () => {
