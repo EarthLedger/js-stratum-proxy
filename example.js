fa3a90dd39d5d144ec88719b725ce5fc5c3deee5
Harrison Beckerich <hbeckeri@purdue.edu>
2019-03-19 09:59:45 -0400

    master updated docker-compose

--- example.js
+++ example.js
@@ -1,11 +1,16 @@
 const net = require('net');
 const StratumProxy = require('./index');
 
+const poolAddress = process.env.POOL_ADDRESS || 'daggerhashimoto.usa.nicehash.com';
+const poolPort = process.env.POOL_PORT || 3353;
+const minerName = process.env.MINER_NAME || 'proxy';
+const address = process.env.MINER_ADDRESS || '3Js4sXXk9Ca8Y52CezUxuZcDugVx8NtrDT';
+
 const app = new StratumProxy({
-	poolAddress: 'daggerhashimoto.usa.nicehash.com',
-	poolPort: 3353,
-	minerName: 'proxy',
-	address: '3Js4sXXk9Ca8Y52CezUxuZcDugVx8NtrDT'
+	poolAddress,
+	poolPort,
+	minerName,
+	address
 });
 
 app.on('log', ({ response }) => console.log(response));
